0a7624c02e88f0ad04f3015c553a7bd311f8e315
phanthienthang <thienthang@hotmail.com>
2015-11-05 11:36:32 +0700

    Update stafftest functions

--- src/test/java/h2/ui/se/mo/staff/StaffTest.java
+++ src/test/java/h2/ui/se/mo/staff/StaffTest.java
@@ -137,53 +137,65 @@ public class StaffTest {
 		
 		//ユーザID - User ID
 		if (iStaff.getUserId() != null)
-			mDriver.findElement(By.id("00N28000006kXGq")).sendKeys(iStaff.getUserId());
+			mDriver.findElement(By.id("00N28000007Hr7G")).sendKeys(iStaff.getUserId());
 		
 		//写真ID - Photo ID
 		if (iStaff.getPhotoId() != null)
-			mDriver.findElement(By.id("00N28000006kXGm")).sendKeys(iStaff.getPhotoId());
+			mDriver.findElement(By.id("00N28000007Hr7C")).sendKeys(iStaff.getPhotoId());
 		
 		//説明 - Description
 		if (iStaff.getDescription() != null)
-			mDriver.findElement(By.id("00N28000006kXGg")).sendKeys(iStaff.getDescription());
+			mDriver.findElement(By.id("00N28000007Hr76")).sendKeys(iStaff.getDescription());
 		
 		//ユーザ - User
 		if (iStaff.getUser() != null)
-			mDriver.findElement(By.id("CF00N28000006kXGr")).sendKeys(iStaff.getUser());
+			mDriver.findElement(By.id("CF00N28000007Hr7H")).sendKeys(iStaff.getUser());
 		
 		//会計担当 - Account
 		if (iStaff.isAccount()){
-			mDriver.findElement(By.id("00N28000006kXGi")).click();
+			mDriver.findElement(By.id("00N28000007Hr78")).click();
 		}
 		
 		//注文担当 - Waiter
 		if (iStaff.isWaiter()){
-			mDriver.findElement(By.id("00N28000006kXGj")).click();
+			mDriver.findElement(By.id("00N28000007Hr79")).click();
 		}
 		
 		//メール - Mail
 		if (iStaff.getMail() != null)
-			mDriver.findElement(By.id("00N28000006kXGh")).sendKeys(iStaff.getMail());
+		{
+			mDriver.findElement(By.id("00N28000007Hr77")).sendKeys(iStaff.getMail());
+		}
 		
 		//電話 - Phone
 		if (iStaff.getPhone() != null)
-			mDriver.findElement(By.id("00N28000006kXGl")).sendKeys(iStaff.getPhone());
+		{
+			mDriver.findElement(By.id("00N28000007Hr7B")).sendKeys(iStaff.getPhone());
+		}
 
 		//携帯 - Mobile
 		if (iStaff.getMobile() != null)
-			mDriver.findElement(By.id("00N28000006kXGk")).sendKeys(iStaff.getMobile());
+		{
+			mDriver.findElement(By.id("00N28000007Hr7A")).sendKeys(iStaff.getMobile());
+		}
 		
 		//誕生日	- Birthday
 		if (iStaff.getBirthday() != null)
-			mDriver.findElement(By.id("00N28000006kXGf")).sendKeys(new SimpleDateFormat("yyyy/mm/dd").format(iStaff.getBirthday()));
+		{
+			mDriver.findElement(By.id("00N28000007Hr75")).sendKeys(new SimpleDateFormat("yyyy/mm/dd").format(iStaff.getBirthday()));
+		}
 		
 		//郵便番号 - Postal Code
 		if (iStaff.getPostalcode() != null)
-			mDriver.findElement(By.id("00N28000006kXGp")).sendKeys(iStaff.getPostalcode());
+		{
+			mDriver.findElement(By.id("00N28000007Hr7F")).sendKeys(iStaff.getPostalcode());
+		}
 		
 		//住所 - Street Address
 		if (iStaff.getStreet() != null)
-			mDriver.findElement(By.id("00N28000006kXGe")).sendKeys(iStaff.getStreet());
+		{
+			mDriver.findElement(By.id("00N28000007Hr74")).sendKeys(iStaff.getStreet());
+		}
 	}
 	
 	

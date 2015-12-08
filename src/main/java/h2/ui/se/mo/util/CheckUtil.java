d41cd2543a5989bfb6fcd822b51e63189fac5422
phanthienthang <thienthang@hotmail.com>
2015-12-08 10:26:23 +0700

    remove third party methods

--- src/main/java/h2/ui/se/mo/util/CheckUtil.java
+++ src/main/java/h2/ui/se/mo/util/CheckUtil.java
@@ -1,8 +1,6 @@
 package h2.ui.se.mo.util;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NotFoundException;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,46 +8,22 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class CheckUtil 
 {
 	/**
-	 * Check element display or not
+	 * Check element with specified ID is display or not
 	 * 
 	 * @param iDriver
 	 * @param iID
 	 * @return
 	 */
-	private static boolean isExistID (WebDriver iDriver, String iID) 
+	public static boolean isExistID (WebDriver iDriver, String iID) 
 	{
-//		try
-//		{
-			WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
-			lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.id(iID)));
-			if (iDriver.findElement(By.id(iID)).isDisplayed())
-			{
-				return true;
-			}
-			
-			return false;
-//		} catch (NotFoundException e){
-//			System.out.println("Element was not found!");
-//			return false;
-//			
-//		} catch (TimeoutException e) {
-//			System.out.println("Loading time is expired!");
-//			new Throwable(e.getMessage());
-//			//return true;
-//		}
-//		return false;
-	}
-	
-	/**
-	 * Check element is display or not
-	 * 
-	 * @param iDriver
-	 * @param iID
-	 * @return
-	 */
-	public static boolean checkExistID(WebDriver iDriver, String iID)
-	{
-		return isExistID(iDriver, iID);
+		WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
+		lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.id(iID)));
+		if (iDriver.findElement(By.id(iID)).isDisplayed())
+		{
+			return true;
+		}
+		
+		return false;
 	}
 	
 	
@@ -60,185 +34,89 @@ public class CheckUtil
 	 * @param iXPath
 	 * @return
 	 */
-	private static boolean isExistXPath(WebDriver iDriver, String iXPath) 
+	public static boolean isExistXPath(WebDriver iDriver, String iXPath) 
 	{
-//		try
-//		{
-			WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
-			lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.xpath(iXPath)));
-			if (iDriver.findElement(By.xpath(iXPath)).isDisplayed())
-			{
-				return true;
-			}
-			
-			return false;
-//		} catch (NotFoundException e){
-//			System.out.println("Element was not found!");
-//			return false;
-//			
-////		} catch (TimeoutException e) {
-//			System.out.println("Loading time is expired!");
-//			new Throwable(e.getMessage());
-//			//return true;
-//		}
-//		return false;
+		WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
+		lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.xpath(iXPath)));
+		if (iDriver.findElement(By.xpath(iXPath)).isDisplayed())
+		{
+			return true;
+		}
+		
+		return false;
 	}
 	
 	/**
-	 * Check the xPath is exist or not in loop.
+	 * @param iDriver
+	 * @param iCssSelector
+	 * @return
+	 */
+	public static boolean isExistCssSelector(WebDriver iDriver, String iCssSelector) 
+	{
+		WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
+		lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.cssSelector(iCssSelector)));
+		if (iDriver.findElement(By.cssSelector(iCssSelector)).isDisplayed())
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * @param iDriver
+	 * @param iName
+	 * @return
+	 */
+	public static boolean isExistName(WebDriver iDriver, String iName) 
+	{
+		WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
+		lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.name(iName)));
+		if (iDriver.findElement(By.name(iName)).isDisplayed())
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Check the element can input value or not.
 	 * 
 	 * @param iDriver
-	 * @param iXPath
+	 * @param iId
 	 * @return
 	 */
-	public static boolean checkExistXPath(WebDriver iDriver, String iXPath) 
+	public static boolean isInputable(WebDriver iDriver, String iId) 
 	{
-		return isExistXPath(iDriver, iXPath);
-	}
-	
-	
-	/**
-	 * @param iDriver
-	 * @param iCssSelector
-	 * @return
-	 */
-	private static boolean isExistCssSelector(WebDriver iDriver, String iCssSelector) 
-	{
-//		try
-//		{
-			WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
-			lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.cssSelector(iCssSelector)));
-			if (iDriver.findElement(By.cssSelector(iCssSelector)).isDisplayed())
-			{
-				return true;
-			}
-			
-			return false;
-			
-//		} catch (NotFoundException e){
-//			System.out.println("Element was not found!");
-//			return false;
-//			
-//		} catch (TimeoutException e) {
-//			System.out.println("Loading time is expired!");
-//			new Throwable(e.getMessage());
-//			//return true;
-//		}
-//		return false;
-	}
-	
-	/**
-	 * @param iDriver
-	 * @param iCssSelector
-	 * @return
-	 */
-	public static boolean checkExistCssSelector(WebDriver iDriver, String iCssSelector) 
-	{
-		return isExistCssSelector(iDriver, iCssSelector);
-	}
-	
-	/**
-	 * @param iDriver
-	 * @param iName
-	 * @return
-	 */
-	private static boolean isExistName(WebDriver iDriver, String iName) 
-	{
-//		try
-//		{
-			WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
-			lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.name(iName)));
-			if (iDriver.findElement(By.name(iName)).isDisplayed())
-			{
-				return true;
-			}
-			
-			return false;
-			
-//		} catch (NotFoundException e){
-//			System.out.println("Element was not found!");
-//			return false;
-//			
-//		} catch (TimeoutException e) {
-//			System.out.println("Loading time is expired!");
-//			new Throwable(e.getMessage());
-//			//return true;
-//		}
-//		
-//		return false;
-	}
-	
-	/**
-	 * @param iDriver
-	 * @param iName
-	 * @return
-	 */
-	public static boolean checkExistName(WebDriver iDriver, String iName) 
-	{
-		return isExistName(iDriver, iName);
-	}
-	
-	private static boolean isInputable(WebDriver iDriver, String iId) 
-	{
-//		try
-//		{
-			WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
-			lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.id(iId)));
-			if (iDriver.findElement(By.id(iId)).isDisplayed())
-			{
-				return true;
-			}
-			
-			return false;
-			
-//		} catch (NotFoundException e){
-//			System.out.println("Element was not found!");
-//			return false;
-//			
-//		} catch (TimeoutException e) {
-//			System.out.println("Loading time is expired!");
-//			new Throwable(e.getMessage());
-//			//return true;
-//		}
-//		
-//		return false;
+		WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
+		lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.id(iId)));
+		if (iDriver.findElement(By.id(iId)).isDisplayed())
+		{
+			return true;
+		}
+		
+		return false;
 		
 	}
 	
-	public static boolean checkInputable(WebDriver iDriver, String iId)
+	/**
+	 * Check the link is exist or not
+	 * 
+	 * @param iDriver
+	 * @param iLink
+	 * @return
+	 */
+	public static boolean isExistLink(WebDriver iDriver, String iLink) 
 	{
-		return isInputable(iDriver, iId);
+		WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
+		lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.linkText(iLink)));
+		if (iDriver.findElement(By.linkText(iLink)).isDisplayed())
+		{
+			return true;
+		}
 		
-	}
-
-	public static boolean checkExistLink(WebDriver iDriver, String iLink) 
-	{
-		return isExistLink(iDriver, iLink);
-	}
-
-	private static boolean isExistLink(WebDriver iDriver, String iLink) 
-	{
-//		try
-//		{
-			WebDriverWait lWaitDriver = new WebDriverWait(iDriver, 30);
-			lWaitDriver.until(ExpectedConditions.elementToBeClickable(By.linkText(iLink)));
-			if (iDriver.findElement(By.linkText(iLink)).isDisplayed())
-			{
-				return true;
-			}
-			
-			return false;
-			
-//		} catch (NotFoundException e){
-//			System.out.println("Element was not found!");
-//			return false;
-//			
-//		} catch (TimeoutException e) {
-//			System.out.println("Loading time is expired!");
-//			new Throwable(e.getMessage());
-//		}
-//		
-//		return false;
+		return false;
 	}
 
 }

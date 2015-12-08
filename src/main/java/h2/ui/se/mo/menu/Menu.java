fa2a219d7d08cb3647e1520d67e66d5640a63300
phanthienthang <thienthang@hotmail.com>
2015-12-08 09:37:48 +0700

    add comment for Menu

--- src/main/java/h2/ui/se/mo/menu/Menu.java
+++ src/main/java/h2/ui/se/mo/menu/Menu.java
@@ -10,21 +10,42 @@ import h2.ui.se.mo.util.PosUtil.BY;
 public class Menu 
 {
 	
+	/**
+	 * Click new button on menu category relative list to create a new menu category
+	 * 
+	 * @param iDriver
+	 */
 	public static void newMenuCategory(WebDriver iDriver)
 	{
 		newHandle(iDriver, "new00N28000007Hr2M");
 	}
 	
+	/**
+	 * Click new button on menu option relative list to create a new menu option
+	 * 
+	 * @param iDriver
+	 */
 	public static void newMenuOption(WebDriver iDriver)
 	{
 		newHandle(iDriver, "new00N28000007Hr2b");
 	}
 	
+	/**
+	 * Click new button on menu service relative list to create a new menu service
+	 * 
+	 * @param iDriver
+	 */
 	public static void newMenuService(WebDriver iDriver)
 	{
 		newHandle(iDriver, "new00N28000007Hr2P");
 	}
 	
+	/**
+	 * Handle method which is used as a common method for click new button on these other relative list.
+	 * 
+	 * @param iDriver
+	 * @param iID
+	 */
 	private static void newHandle(WebDriver iDriver, String iID) 
 	{
 		try
@@ -37,6 +58,13 @@ public class Menu
 		}
 	}
 	
+	/**
+	 * Add one menu relation record
+	 * 
+	 * @param iDriver
+	 * @param iID
+	 * @param iData
+	 */
 	public static void addMenuRelation(WebDriver iDriver, String iID, String iData)
 	{
 		//CF00N28000007Hr2M - Menu Category
@@ -60,12 +88,26 @@ public class Menu
 		}
 	}
 	
+	/**
+	 * Multiple adding menu relation records for only one specified menu
+	 * 
+	 * @param iDriver
+	 * @param iID
+	 * @param iData
+	 */
 	public static void addMenuRelationList(WebDriver iDriver, String iID, String...iData) {
 		for (int i = 0; i < iData.length; i++) {
 			addNewRelation(iDriver, iID, iData[i]);
 		}
 	}
 	
+	/**
+	 * Continuous adding data in Salesforce by click save and new
+	 * 
+	 * @param iDriver
+	 * @param iID
+	 * @param iData
+	 */
 	private static void addNewRelation(WebDriver iDriver, String iID, String iData) {
 		try
 		{

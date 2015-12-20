8a5a168b063609f7d855d9b4ede1377528595d2b
phanthienthang <thienthang@hotmail.com>
2015-12-20 23:03:31 +0700

    re-factor

--- src/main/java/h2/ui/se/mo/util/PosConstant.java
+++ src/main/java/h2/ui/se/mo/util/PosConstant.java
@@ -8,5 +8,11 @@ public class PosConstant {
 	public static final String MO_LOGIN_PASSWORD = "mo.login.password";
 	
 	public static final String MESSAGE_ERROR_ALERT_HANDLE = "ERROR HANDLE ALERT";
+	
+	public static final String SFDC_BTN_ID_ALL_TAB = "AllTab_Tab";
+	public static final String SFDC_BTN_ID_LOGIN = "button-login";
+	public static final String SFDC_BTN_NAME_NEW = "new";
+	public static final String SFDC_URL = "https://www.salesforce.com";
+	
 
 }
